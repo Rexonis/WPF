fa70aa2b48e2417d0c5ce2ab128282c985a16435
Rexonis <140373571+Rexonis@users.noreply.github.com>
2023-07-24 14:54:59 +0500

    Add files via upload

---
+++ ApplicationContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Registration
+{
+    internal class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;port=3306;user=root;password=root;database=Berestov_register", new MySqlServerVersion(new Version(8, 0, 0)));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasOne(e => e.Role).WithMany(e => e.Users).HasForeignKey(e => e.RoleId);
+        }
+        public static User Sravn(string current_Login, string current_Pass)
+        {
+            using (var context = new ApplicationContext())
+            {
+                return context.Users.FirstOrDefault(t => t.Login == current_Login && t.Password == current_Pass);
+            };
+
+        }
+    }
+}
---
+++ DBEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Registration
+{
+    internal class Role
+    {
+        [Required, Key]
+        public Int32 Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public List<User> Users { get; set; }
+    }
+    internal class User
+    {
+        [Required, Key]
+        public Int32 Id { get; set; }
+        public int RoleId { get; set; }
+        [Required]
+        public String Login { get; set; }
+        [Required]
+        public String Password { get; set; }
+        [Required]
+        public String UserName { get; set; }
+        [Required]
+        public Role Role { get; set; }
+    }
+    internal class DBEntity
+    {
+    }
+}
